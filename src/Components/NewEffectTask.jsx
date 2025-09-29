b3daf6f3548c814447cefdf4dc159601344f2c5c
farhan5548 <farhanfareed450@gmail.com>
2025-09-29 11:39:49 +0500

    ok

--- src/Components/NewEffectTask.jsx
+++ src/Components/NewEffectTask.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import { useEffect, useState } from "react";
 
 function NewEffectTask() {
-  return (
-    <div>
-        
-    </div>
-  )
+    const [count, setCount] = useState(60);
+    useEffect(() => {
+        if (count === 0) return
+        const interval = setInterval(() => {
+            setCount((prev) => prev - 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [count]);
+
+    return (
+        <div className='border-8 border-black rounded-[2rem] mx-[30rem] h-[20vh] w-[15vw] flex items-center justify-center'>
+            <p>{count > 0 ? count : "Time's up!"}</p>
+        </div>
+    )
 }
 
 export default NewEffectTask
