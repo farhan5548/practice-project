297de694d3ef929834c42395bb00839dd9948bd8
farhan5548 <farhanfareed450@gmail.com>
2025-09-29 11:14:53 +0500

    added

--- src/Components/Taskeffect.jsx
+++ src/Components/Taskeffect.jsx
@@ -8,6 +8,22 @@ function Taskeffect() {
         setCount(text.length);
     }, [text]);
 
+    const clr = ["red", "blue", "green", "yellow", "white", "pink", "purple", "orange"];
+    const [colr, setColr] = useState(clr[0]);
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((prevIndex) => (prevIndex + 1) % clr.length);
+        }, 1250);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    useEffect(() => {
+        setColr(clr[index]);
+    }, [index]);
+
+
     return (
         <>
             <div className='border-8 border-black rounded-[5rem] h-[50vh] m-5 p-6'>
@@ -30,12 +46,13 @@ function Taskeffect() {
 
 
 
-            <div className='border-8 border-black rounded-[5rem] h-[50vh] m-5 p-5 items-center flex justify-center flex-col'>
+            <div className='border-8 border-black rounded-[5rem] h-[50vh] m-5 p-5 items-center flex justify-center flex-col'
+                style={{ backgroundColor: colr }}>
                 <h1 className='text-center font-bold text-3xl pt-4'>useEffect Hook Practice</h1>
                 <p className='text-center mt-5 text-lg'>
-                    Current Color is: <span className='font-bold'>{}</span>
+                    Current Color is : <span className='font-bold'>{colr}</span>
                 </p>
-            </div>  
+            </div>
         </>
     )
 }
