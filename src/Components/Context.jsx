0cb967d97c81f46145a8dcecbcedefc3afc0bef9
farhan5548 <farhanfareed450@gmail.com>
2025-10-02 11:15:37 +0500

    useContext practice

---
+++ src/Components/Context.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Context() {
+    return (
+        <div className="font-bold border-8  rounded-3xl bg-white p-3 border-black text-center mx-[31.5rem]">
+
+        </div>
+    )
+}
+
+export default Context
